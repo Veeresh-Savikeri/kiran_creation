891beb139b885fb048fffae86da832c6336b9b47
RohanKM16 <rohanmelagiri@gmail.com>
2025-04-13 00:32:24 +0530

    carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import pto from '../imgs/visitingCard.webp';
+import pto1 from '../imgs/pto1.webp';
+import pto2 from '../imgs/pto2.webp';
+import '../Css/carousel.css';
 
 export default function Carousel() {
     return (
-        <div className="position-relative">
-            <img src={pto} alt="" className="h-20 img-fluid" style={{ width: '100vw',minHeight:"300px" }} />
-           
+        <div className="d-flex justify-content-around align-items-center flex-row">
+            <div className="c1 position-relative">
+                <img className="w-100 h-100" src={pto1} alt="" />
+                <div className="shop position-absolute translate-middle text-center p-5 text-dark rounded">
+                    <h1 className="fw-bold fs-4">Visiting Cards</h1>
+                    <p className="fs-6">100 Visiting Cards at Rs 200</p>
+                    <button className="btn btn-dark p-2 fs-5 fw-bold">Shop Now</button>
+                </div>
+            </div>
+            <div className="c1">
+                <img className="w-100 h-100" src={pto2} alt="" />
+            </div>
         </div>
     );
 }
