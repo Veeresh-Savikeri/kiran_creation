750f131684ef2e96c4678d5b49d57346749dd968
Veeresh Savikeri <veeresh2004m@gmail.com>
2025-04-12 19:31:15 +0530

    scrolling cards

--- src/Screens/StdVcard.js
+++ src/Screens/StdVcard.js
@@ -6,10 +6,20 @@ import Rel_card from "../Components/Rel_card";
 import "../Css/std_vcard.css";
 
 export default function StdVcard() {
+  function scrollLeft() {
+    const container = document.querySelector(".scroll-container");
+    container.scrollBy({ left: -700, behavior: "smooth" });
+  }
+
+  function scrollRight() {
+    const container = document.querySelector(".scroll-container");
+    container.scrollBy({ left: 700, behavior: "smooth" });
+  }
+
   return (
     <div>
       <Navbar />
-      
+
       <div className="bg-light " style={{ minHeight: "100vh" }}>
         <div className="d-flex flex-wrap justify-content-between align-items-start container shadow-lg mt-5">
           {/* Left Box */}
@@ -211,16 +221,32 @@ export default function StdVcard() {
           </div>
         </div>
       </div>
-      <h1 className="text-center mt-5 fw-bold">Related Products</h1>
-      <div className="cards d-flex justify-content-center align-items-center marquee-container">
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
-        <Rel_card />
+
+      <div>
+        <h1 className="text-start ms-5 mt-5 fw-bold">Related Products</h1>
+        <div className="scroll-wrapper position-relative">
+          <button className="btn btn-primary scroll-left" onClick={scrollLeft}>
+            <i className="fa fa-chevron-left" aria-hidden="true"></i>
+          </button>
+
+          <div className="scroll-container d-flex align-items-center">
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+            <Rel_card />
+          </div>
+
+          <button
+            className="btn btn-primary scroll-right"
+            onClick={scrollRight}
+          >
+            <i className="fa fa-chevron-right" aria-hidden="true"></i>
+          </button>
+        </div>
       </div>
     </div>
   );
